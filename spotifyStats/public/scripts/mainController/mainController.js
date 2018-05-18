ef991deba2df8fb5d71e58b6c8e0a058089be565
Dominik <Dominik@DESKTOP-LSB8LL2>
2018-05-18 20:31:45 +0200

    connect with spotify <only https>

--- spotifyStats/public/scripts/mainController/mainController.js
+++ spotifyStats/public/scripts/mainController/mainController.js
@@ -1,6 +1,8 @@
 var mainController = /** @class */ (function () {
-    function mainController($scope) {
-        console.log("xDDD");
+    function mainController($scope, $http) {
+        $scope.testMessage = function () {
+            $http.get("http://localhost:1337/login").then(function (value) { return console.log(value.data); }, function (err) { return console.error(err); });
+        };
     }
     return mainController;
 }());
--- spotifyStats/public/scripts/mainController/mainController.ts
+++ spotifyStats/public/scripts/mainController/mainController.ts
@@ -1,10 +1,17 @@
 ﻿interface mainInterface {
-
+    testMessage(): void;
 }
 
 class mainController {
-    constructor($scope: mainInterface) {
+    constructor($scope: mainInterface, $http: angular.IHttpService) {
+        $scope.testMessage = () => {
+            $http.get("http://localhost:1337/login").then(
+                value => console.log(value.data),
+                err => console.error(err)
+            );
+        }
     }
+
 }
 
 app.controller('mainController', mainController);
